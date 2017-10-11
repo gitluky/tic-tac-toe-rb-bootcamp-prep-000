5e395b1673a766643b5ac35dc6000452ffdd7c99
gitluky <jluk87@gmail.com>
2017-10-11 18:07:24 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -30,3 +30,17 @@ def winner(board)
     board[winning_combo.first]
   end
 end
+
+def turn_count(board)
+  count = 0
+  board.each do |location|
+    if location != " "
+      count+=1
+    end
+  end
+  return count
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X": "O"
+end
